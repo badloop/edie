9bfd966af8c20ec282fbf01be4ec2163bba39d16
Aaron Dunlap <aaronmdunlap@icloud.com>
2023-10-26 10:11:10 -0500

    fix: Updating pre-commit; adding structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// use std::fs::{self, File};
+// use std::io::{Read, Seek, SeekFrom};
+
+#[derive(Debug, Default)]
+struct Item {
+    name: String,
+    count: i8,
+}
+
+#[derive(Debug, Default)]
+struct Inventory {
+    items: Option<Vec<Item>>,
+}
+
+#[derive(Debug, Default)]
+struct Suit {
+    name: Option<String>,
+    suit_type: Option<String>,
+    inventory: Option<Inventory>,
+}
+
+#[derive(Debug, Default)]
+struct Commander {
+    name: Option<String>,
+    suit: Option<Suit>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Commander: {:?}", Commander::default());
 }
